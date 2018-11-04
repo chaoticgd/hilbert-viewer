a78c4cb8c05a34575f5c074d0c94c2f3643a6b5a
chaoticgd <chaoticgd@protonmail.com>
2018-11-04 19:26:18 +0000

    Image loading

--- viewer.js
+++ viewer.js
@@ -11,18 +11,39 @@ window.addEventListener('load', function() {
 	var canvas = document.getElementById('viewport');
 	var context = canvas.getContext('2d');
 
-	function openImage() {
+	var image = new Image();
+	const padding = 32;
+
+	function openImage(event) {
 		var reader = new FileReader();
 		reader.addEventListener('load', function(event) {
-			var image = new Image();
-			image.addEventListener('load', function() {
-				
-			});
 			image.src = event.target.result;
 		});
+		reader.readAsDataURL(event.target.files[0]);
 	}
 
-	document.getElementById('open-image-button').addEventListener('click', function() {
-		
-	});
+	document.getElementById('open-file-button').addEventListener('change', openImage);
+
+	function draw() {
+		canvas.width = Math.max(canvas.clientWidth, canvas.innerWidth || 0);
+		canvas.height = Math.max(canvas.clientHeight, canvas.innerHeight || 0);
+
+		var imageSize = {};
+		if(canvas.width > canvas.height) {
+			imageSize.x = canvas.height - 2 * padding;
+			imageSize.y = canvas.height - 2 * padding;
+		} else {
+			imageSize.x = canvas.width - 2 * padding;
+			imageSize.x = canvas.width - 2 * padding;
+		}
+
+		context.drawImage(
+			image,
+			canvas.width / 2 - imageSize.x / 2,
+			canvas.height / 2 - imageSize.y / 2,
+			imageSize.x,
+			imageSize.y);
+	}
+
+	window.setInterval(draw, 1000 / 30);
 });
